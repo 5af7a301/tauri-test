16d31389999bafa72a0a2b886ba3f964e65f7477
5af7a301 <129606256+5af7a301@users.noreply.github.com>
2023-04-01 23:25:15 +0000

    update

--- tauri-app/src/App.vue
+++ tauri-app/src/App.vue
@@ -28,9 +28,7 @@ import Greet from "./components/Greet.vue";
       +
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
       +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
+      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">Tauri</a>
       +
       <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">rust-analyzer</a>
     </p>
